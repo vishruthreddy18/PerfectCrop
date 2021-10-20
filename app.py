a83e3f164e371545b1417e8b6eb09f8daf6761d1
Ishan Agarwal <ishanagarwal1605@gmail.com>
2021-10-20 12:53:12 +0530

    initial deploy

--- app.py
+++ app.py
@@ -554,6 +554,3 @@ def map_display():
     # start_coords = (46.9540700, 142.7360300)
     # folium_map = folium.Map(location=start_coords, zoom_start=14)
     return m._repr_html_()
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
